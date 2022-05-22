e633de91c1692fe2ffe248a105fef4444599f9dd
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-05-22 19:18:05 +0600

    homecomplete

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,17 +4,15 @@ import background01 from '../image/background01.png'
 const Banner = () => {
     return (
         < >
-            <div className="hero h-[60vh] bg-accent">
-                <div className="hero-content flex-col lg:flex-row">
-                    
+            <div className="hero  lg:h-[60vh] bg-accent mt-16">
+                <div className="hero-content flex-col lg:flex-row">    
                     <div>
-                        <h1 className="text-5xl font-bold">
-                            Manufacturer Item!</h1>
+                        <h1 className="text-4xl font-bold"> Manufacturer Item!</h1>
                         <p className="py-6 max-w-xl">A manufacturer is a person or company that produces finished goods from raw materials by using various tools, equipment, and processes, and then sells the goods ...</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
 
-                    <div className='h-[60vh]'>
+                    <div className='lg:h-[60vh] shrink-0'>
                     <img src={background01} className="h-full" alt='background'  />
                     </div>
                 </div>
--- src/Pages/Navber/Navber.js
+++ src/Pages/Navber/Navber.js
@@ -10,7 +10,7 @@ const Navber = () => {
             <li> <Link to="/Login"> Login </Link>  </li>
         </>
     return (
-        <div className="navbar  bg-base-100 ">
+        <div className="navbar sticky top-0 -z-50  bg-base-100 ">
         <div className="navbar-start  ">
             <div className="dropdown ">
                 <label tabIndex="0" className="btn btn-ghost lg:hidden ">
